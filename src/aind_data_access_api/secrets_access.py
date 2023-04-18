d95070a97391bac7943f7a432aa0668f78bcd13a
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2023-04-18 15:10:09 -0700

    linters

--- src/aind_data_access_api/secrets_access.py
+++ src/aind_data_access_api/secrets_access.py
@@ -6,21 +6,21 @@ from botocore.exceptions import ClientError
 
 def get_secret(secret_name: str):
     """Retrieves specified secret if user has proper aws permissions"""
-    # Create a secrets manager client
-    client = boto3.client('secretsmanager')
+    # Create a Secrets Manager client
+    client = boto3.client("secretsmanager")
     try:
         response = client.get_secret_value(SecretId=secret_name)
     except ClientError:
         return None
     finally:
         client.close()
-    return json.loads(response['SecretString'])['password']
+    return json.loads(response["SecretString"])["password"]
 
 
 def get_parameter(parameter_name: str):
     """Retrieves specified parameter if user has proper aws permissions"""
     # Create a Systems Manager client
-    client = boto3.client('ssm')
+    client = boto3.client("ssm")
 
     try:
         response = client.get_parameter(Name=parameter_name)
@@ -28,4 +28,4 @@ def get_parameter(parameter_name: str):
         return None
     finally:
         client.close()
-    return response['Parameter']['Value']
+    return response["Parameter"]["Value"]
--- tests/test_secrets_access.py
+++ tests/test_secrets_access.py
@@ -1,75 +1,95 @@
+"""Test secrets_access module."""
 import unittest
 from unittest.mock import patch, Mock
 from botocore.exceptions import ClientError
 from aind_data_access_api.secrets_access import get_secret, get_parameter
 
 
-class TestMyModule(unittest.TestCase):
+class TestSecretAccess(unittest.TestCase):
+    """Test methods in secrets_access module"""
 
-    @patch('boto3.client')
+    @patch("boto3.client")
     def test_get_secret_success(self, mock_boto3_client):
+        """Tests that secret is retrieved as expected"""
         # Mock the Secrets Manager client and response
         mock_client = Mock()
         mock_boto3_client.return_value = mock_client
-        mock_secret_string = '{"username": "admin", "password": "secret_password"}'
-        mock_response = {'SecretString': mock_secret_string}
+        mock_secret_string = (
+            '{"username": "admin", "password": "secret_password"}'
+        )
+        mock_response = {"SecretString": mock_secret_string}
         mock_client.get_secret_value.return_value = mock_response
 
         # Call the get_secret method with a mock secret name
-        secret_name = 'my_secret'
+        secret_name = "my_secret"
         secret_value = get_secret(secret_name)
 
         # Assert that the Secrets Manager client was called with the correct arguments
-        mock_boto3_client.assert_called_with('secretsmanager')
+        mock_boto3_client.assert_called_with("secretsmanager")
         mock_client.get_secret_value.assert_called_with(SecretId=secret_name)
 
         # Assert that the secret value returned matches the expected value
-        expected_value = 'secret_password'
+        expected_value = "secret_password"
         self.assertEqual(secret_value, expected_value)
 
-    @patch('boto3.client')
+    @patch("boto3.client")
     def test_get_secret_permission_denied(self, mock_boto3_client):
-        mock_boto3_client.return_value.get_secret_value.side_effect = \
+        """Tests that secret is not returned when incorrect aws permissions"""
+        mock_boto3_client.return_value.get_secret_value.side_effect = (
             ClientError(
-                {'Error': {'Code': 'AccessDeniedException', 'HTTPStatusCode': 403}},
-                'get_secret_value'
+                {
+                    "Error": {
+                        "Code": "AccessDeniedException",
+                        "HTTPStatusCode": 403,
+                    }
+                },
+                "get_secret_value",
             )
+        )
         # Call the method to get the secret
-        result = get_secret('my_secret')
+        result = get_secret("my_secret")
 
         # Check that the method returns None
         self.assertIsNone(result)
 
-    @patch('boto3.client')
+    @patch("boto3.client")
     def test_get_parameter_success(self, mock_boto3_client):
+        """ "Tests that parameter is retrieved as expected"""
         # Mock the Systems Manager client and response
         mock_client = Mock()
         mock_boto3_client.return_value = mock_client
-        mock_parameter_value = 'my_parameter_value'
-        mock_response = {'Parameter': {'Value': mock_parameter_value}}
+        mock_parameter_value = "my_parameter_value"
+        mock_response = {"Parameter": {"Value": mock_parameter_value}}
         mock_client.get_parameter.return_value = mock_response
 
         # Call the get_parameter method with a mock parameter name
-        parameter_name = '/my/parameter/name'
+        parameter_name = "/my/parameter/name"
         parameter_value = get_parameter(parameter_name)
 
         # Assert that the Systems Manager client was called with the correct arguments
-        mock_boto3_client.assert_called_with('ssm')
+        mock_boto3_client.assert_called_with("ssm")
         mock_client.get_parameter.assert_called_with(Name=parameter_name)
 
         # Assert that the parameter value returned matches the expected value
-        expected_value = 'my_parameter_value'
+        expected_value = "my_parameter_value"
         self.assertEqual(parameter_value, expected_value)
 
-    @patch('boto3.client')
+    @patch("boto3.client")
     def test_get_parameter_permission_denied(self, mock_boto3_client):
-        mock_boto3_client.return_value.get_secret_value.side_effect = \
+        """Tests that parameter is not returned when incorrect aws permissions"""
+        mock_boto3_client.return_value.get_secret_value.side_effect = (
             ClientError(
-                {'Error': {'Code': 'AccessDeniedException', 'HTTPStatusCode': 403}},
-                'get__parameter'
+                {
+                    "Error": {
+                        "Code": "AccessDeniedException",
+                        "HTTPStatusCode": 403,
+                    }
+                },
+                "get__parameter",
             )
+        )
         # Call the method to get the secret
-        result = get_secret('my_secret')
+        result = get_secret("my_secret")
 
         # Check that the method returns None
         self.assertIsNone(result)
