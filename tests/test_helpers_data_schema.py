4f340d3554c5654d6b0d9dad0414b0b127ec2642
Dan Birman <danbirman@gmail.com>
2024-11-05 11:23:05 -0800

    refactor: rename test files, use relative paths

--- tests/test_helpers_data_schema.py
+++ tests/test_helpers_data_schema.py
@@ -9,6 +9,10 @@ from aind_data_access_api.helpers.data_schema import (
     get_quality_control_by_name,
 )
 from aind_data_schema.core.quality_control import QualityControl
+import os
+
+TEST_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
+TEST_HELPERS_DIR = TEST_DIR / "resources" / "helpers"
 
 
 class TestUtilDataSchema(unittest.TestCase):
@@ -17,13 +21,12 @@ class TestUtilDataSchema(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         """Set up the class by extracting contents from example files."""
-        file_dir = Path("./tests/resources/helpers")
 
-        valid_path = file_dir / "quality_control.json"
+        valid_path = TEST_HELPERS_DIR / "quality_control.json"
         with valid_path.open("r") as f:
             cls.example_quality_control = json.load(f)
 
-        invalid_path = file_dir / "quality_control_invalid.json"
+        invalid_path = TEST_HELPERS_DIR / "quality_control_invalid.json"
         with invalid_path.open("r") as f:
             cls.example_quality_control_invalid = json.load(f)
 
